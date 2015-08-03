5604caa54d3288bc1e856ef93d3e33e126e4d6b1
PC <PC@pratha.gateway.2wire.net>
2015-08-03 11:23:42 -0400

    User Register

---
+++ src/java/phonestore/controller/UserPasswordUpdateServlet.java
@@ -0,0 +1,39 @@
+package phonestore.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import phonestore.model.User;
+
+/**
+ *
+ * @author pratha
+ */
+public class UserPasswordUpdateServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String loginId = req.getParameter("loginId");
+        String password = req.getParameter("userPassword");
+        
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        
+        User user = new User();
+        
+        boolean check = user.updateUserPassword(loginId, password);
+        
+        if(check){
+            out.println("<h3>Password Updated</h3>");
+            out.println("<a href=\"UserAccountSettings.jsp\">Account settings</a>");
+        }
+        else{
+            out.println("<h3>Password NOT Updated</h3>");
+            out.println("<a href=\"UserAccountSettings.jsp\">Account settings</a>");
+        }
+    }
+    
+}
