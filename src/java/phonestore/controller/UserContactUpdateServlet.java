98aaf4f4bcba6b9e3ab2648feb2dc3b5cb172019
PC <PC@pratha.gateway.2wire.net>
2015-08-03 12:06:03 -0400

    User contact update servlet.

---
+++ src/java/phonestore/controller/UserContactUpdateServlet.java
@@ -0,0 +1,43 @@
+
+package phonestore.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import phonestore.model.User;
+
+/**
+ *
+ * @author pratha
+ */
+public class UserContactUpdateServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String countryCode = req.getParameter("countryCode");
+        String contactNum = req.getParameter("contactNum");
+        
+        String loginId = req.getParameter("loginId");
+        String userMob = countryCode+"-"+contactNum;
+        
+        User user = new User();
+        
+        boolean check = user.updateUserContact(userMob, loginId);
+        
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        
+        if(check){
+            out.println("<h3>Contact Number Updated</h3>");
+            out.println("<a href=\"UserAccountSettings.jsp\">Account settings</a>");
+        }
+        else{
+            out.println("<h3>Contact Number NOT Updated</h3>");
+            out.println("<a href=\"UserAccountSettings.jsp\">Account settings</a>");
+        }
+    }
+    
+}
